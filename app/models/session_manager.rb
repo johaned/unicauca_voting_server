991b1f1e6cdb5d3ca6879ed355b177ac946df1de
Johaned <johan.tique@codescrum.com>
2014-03-01 20:56:09 -0500

    add SessionManager

---
+++ app/models/session_manager.rb
@@ -0,0 +1,80 @@
+class SessionManager
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :token,              type: String
+  field :devices_authorized, type: Array, default: []
+
+  belongs_to :user
+
+  validates :token, :uniqueness => true
+
+  # defines a flow to authenticate and associate a SessionManager instance with
+  # the current user account. Also, it pushes a device only if it is not already
+  # referenced
+  #
+  # @param username [String]
+  # @param password [String]
+  # @param user_agent [String], device that user connects
+  #
+  # @return [Hash], with result of process
+  # e.g.
+  # {
+  #  code: 200,
+  #  msg: 'session was approved successfully',
+  #  token: 'WnMqJzPxBsIdokijwcKKUIXopnFhqWzFVnUxYTfiRGOoTnKurO'
+  # }
+  def self.authenticate(username, password, user_agent)
+    response = {}
+    user = user_account username, password
+    if user.present?
+      session_manager = user.session_manager.present? ? user.session_manager : associate_token(user)
+      session_manager.associate_user_agent user_agent
+      response[:code] = 200
+      response[:msg] = 'session was approved successfully'
+      response[:token] = session_manager.token
+    else
+      response[:code] = 401
+      response[:msg] = 'session was rejected'
+    end
+    response
+  end
+
+  # associates a new device to session manager only if this is not already associated
+  #
+  # @param user_agent [String], device's name related to new session
+  def associate_user_agent(user_agent)
+    update_attribute(:devices_authorized, self.devices_authorized << user_agent) unless self.devices_authorized.try(:include?, user_agent)
+  end
+
+  # Checks if username:password pair exists
+  #
+  # @param username [string]
+  # @param password [String]
+  #
+  # @return [User]
+  def self.user_account(username, password)
+    password_encrypted = Digest::SHA1.hexdigest password
+    User.where(username: username, password_encrypted: password_encrypted).first
+  end
+
+  # creates a new session manager for User account
+  #
+  # @param user [User]
+  #
+  # @return [SessionManager]
+  def self.associate_token(user)
+    user.create_session_manager(token: generate_token)
+  end
+
+  # Generates a new unique token
+  #
+  # @return [String]
+  def self.generate_token
+    input = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+    token = (0...50).map { input[rand(input.length)] }.join
+    generate_token if self.where(token: token).exists?
+    token
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,23 @@ class User
   field :username,           type: String
   field :password_encrypted, type: String
 
+  has_one :session_manager
+
+  validates :username, :uniqueness => true
+
+  # It sets a new real password as a password encrypted
+  #
+  # @param password [String]
   def password=(password)
     self.password_encrypted = Digest::SHA1.hexdigest password
   end
 
-  def password_encrypted=(encrypted_password)
-    if /^\b([a-f0-9]{40})\b/ === encrypted_password
-      self[:password_encrypted] = encrypted_password
+  # It sets a new password encrypted
+  #
+  # @param password_encrypted [String], password encrypted must be using SHA1 algorithm
+  def password_encrypted=(password_encrypted)
+    if /^\b([a-f0-9]{40})\b/ === password_encrypted
+      self[:password_encrypted] = password_encrypted
     else
       raise 'password must be encrypted in SHA1'
     end
