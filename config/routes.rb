e0c00e721db4897506ae19835b71951aa388feb6
Johaned <johan.tique@codescrum.com>
2014-03-18 17:44:48 -0500

    update routes with question actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 VotingServer::Application.routes.draw do
   post '/sign_in', to: 'session#sign_in'
   post '/user/name', to: 'user#name'
+  post '/questions/current_question', to: 'questions#current_question'
+  post '/questions/vote', to: 'questions#vote'
+  post '/questions/results', to: 'questions#results'
+
 end
