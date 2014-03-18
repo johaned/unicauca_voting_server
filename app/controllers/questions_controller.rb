92df23d984983a4aacd9ab3be45313d4ee8637a8
Johaned <johan.tique@codescrum.com>
2014-03-18 17:20:52 -0500

    add current_question, vote and results action in QuestionController controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,25 @@ class QuestionsController < ApplicationController
   before_filter :require_login
 
   def current_question
+    current_question = Question.current_question
+    respond_to do |format|
+      format.json { render json: (current_question.caption || 'No question...') }
+    end
   end
 
   def vote
+    current_question = Question.current_question
+    current_question.vote(params[:vote])
+    respond_to do |format|
+      format.json { render json: 'Voted' }
+    end
   end
 
   def results
+    current_question = Question.current_question
+    respond_to do |format|
+      format.json { render json: current_question.results }
+    end
   end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,9 @@ class UserController < ApplicationController
   before_filter :require_login
 
   def name
-    current_user.name
+    respond_to do |format|
+      format.json { render json: (current_user.name || '') }
+    end
   end
 
 end
